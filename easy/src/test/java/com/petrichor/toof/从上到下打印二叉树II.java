33b4358c073dafd7ad75a84cdadf0d54fb98ba64
jianghuan <2959543063@qq.com>
2022-01-26 16:58:45 +0800

    2022/1/26

---
+++ easy/src/test/java/com/petrichor/toof/从上到下打印二叉树II.java
@@ -0,0 +1,60 @@
+package com.petrichor.toof;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @Author jh
+ * @Description
+ * @Date created in 9:45 2022/1/26
+ */
+public class 从上到下打印二叉树II {
+
+    /**
+     * 大规模 从上到下打印所有结点
+     * 小规模 根root将俩结点添加到list;
+     * 出口   root == null;
+     */
+
+    @Test
+    public void main() {
+        TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        List<List<Integer>> res = levelOrder(root);
+        for (List<Integer> list:res) {
+            for (Integer i :list) {
+                System.out.print(i + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            ArrayList<Integer> list = new ArrayList<>();
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            res.add(list);
+        }
+        return res;
+    }
+
+}
--- src/test/java/org/petrichor/AppTest.java
+++ src/test/java/org/petrichor/AppTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit basic for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
