27fdd3e8d5f051c62c230aa4a80719d4eeef589c
jianghuan <2959543063@qq.com>
2023-01-29 09:29:03 +0800

    单词接龙

--- easy/src/test/java/com/petrichor/array/单词接龙.java
+++ easy/src/test/java/com/petrichor/array/单词接龙.java
@@ -1,6 +1,8 @@
 package com.petrichor.array;
 
-import java.util.List;
+import org.junit.Test;
+
+import java.util.*;
 
 /**
  * @Author jh
@@ -41,22 +43,56 @@ public class 单词接龙 {
      * wordList 中的所有字符串 互不相同
      */
 
-    /**
-     *                     hit
-     *                     hot
-     *            dot              lot
-     *       dog     lot        dot   log
-     *    log  cog     log
-     *               dog
-     */
+//      ["hot","dot","dog","lot","log","cog"]
 
+//                          hit
+//                       hot
+//                dot                    lot
+//           dog      lot              dot   log
+//        log  cog   log            dog     dog cog
+//      cog        dog           cog log   cog
+//               cog
+    @Test
+    public void main() {
 
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        return 0;
+        List<String> list = Arrays.asList("hot", "dot", "dog", "lot", "log", "cog");
+        HashSet<String> wordList = new HashSet<>(list);
+        System.out.println(ladderLength("hit", "cog", wordList));
+
+//        List<String> wordList = Arrays.asList("hot", "dot", "dog", "lot", "log", "cog");
+//        System.out.println(ladderLength("hit", "cog", wordList));
+//        List<String> wordList = Arrays.asList("hot", "dog");
+//        System.out.println(ladderLength("hot", "dog", wordList));
     }
 
-    public int[] dps(String endWord, List<String> wordList, List<String> tmp,int[] result) {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
 
+        ArrayList<String> tmp = new ArrayList<>();
+        int[] result = new int[1];
+        result[0] = 0;
+        tmp.add(beginWord);
+        dps(endWord, wordList, tmp, result);
+        return result[0];
+    }
+
+    public void dps(String endWord, List<String> wordList, List<String> tmp, int[] result) {
+
+
+        if (endWord.equals(tmp.get(tmp.size() - 1))) {
+            if (result[0] == 0) {
+                result[0] = tmp.size();
+            } else if (tmp.size() < result[0]) {
+                result[0] = tmp.size();
+                return;
+            }
+        } else {
+            if (result[0] != 0 && tmp.size() >= result[0]) {
+                return;
+            }
+        }
 
 
         for (String s : wordList) {
@@ -64,11 +100,12 @@ public class 单词接龙 {
             if (tmp.contains(s))
                 continue;
 
-
             //判断是否只有一个字母不同
-            if (isSame(s, wordList.get(wordList.size() - 1)))
+            if (isSame(s, tmp.get(tmp.size() - 1))) {
                 tmp.add(s);
-
+                dps(endWord, wordList, tmp, result);
+                tmp.remove(tmp.size() - 1);
+            }
         }
     }
 
@@ -83,4 +120,40 @@ public class 单词接龙 {
         }
         return true;
     }
+
+    public int ladderLength(String start, String end, HashSet<String> dict) {
+        if (dict.size() == 0)
+            return 0;
+        dict.add(end);
+        LinkedList<String> wordQueue = new LinkedList<>();
+        LinkedList<Integer> distanceQueue = new LinkedList<>();
+        wordQueue.add(start);
+        distanceQueue.add(1);
+        //track the shortest path
+        int result = Integer.MAX_VALUE;
+        while (!wordQueue.isEmpty()) {
+            String currWord = wordQueue.pop();
+            Integer currDistance = distanceQueue.pop();
+            if (currWord.equals(end)) {
+                result = Math.min(result, currDistance);
+            }
+            for (int i = 0; i < currWord.length(); i++) {
+                char[] currCharArr = currWord.toCharArray();
+                for (char c = 'a';c <= 'z';c++){
+                    currCharArr[i] = c;
+                    String newWord = new String(currCharArr);
+                    if (dict.contains(newWord)) {
+                        wordQueue.add(newWord);
+                        distanceQueue.add(currDistance + 1);
+                        dict.remove(newWord);
+                    }
+                }
+            }
+        }
+        if (result < Integer.MAX_VALUE)
+            return result;
+        else
+            return 0;
+    }
+
 }
