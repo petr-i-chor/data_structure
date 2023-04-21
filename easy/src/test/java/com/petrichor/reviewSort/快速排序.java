936f5b98d6348ed99c56b9e9da6ac72bc992c9a6
jianghuan <2959543063@qq.com>
2023-04-21 14:16:31 +0800

    快速排序

--- easy/src/test/java/com/petrichor/reviewSort/快速排序.java
+++ easy/src/test/java/com/petrichor/reviewSort/快速排序.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class 快速排序 {
     @Test
     public void main() {
-        int[] nums = {13, 2, 32, 5, 7, 3, 71, 8, 12,1, 421, 43, 1};
+        int[] nums = {13, 2, 32, 5, 7, 3, 71, 8, 12, 1, 421, 43, 1};
         quickSort(nums);
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + "\t");
@@ -19,9 +19,35 @@ public class 快速排序 {
 
     public void quickSort(int[] nums) {
 
-        dps(nums, 0, nums.length - 1);
-        return;
+        quickSort(nums, 0, nums.length - 1);
     }
+
+    private void quickSort(int nums[], int left, int right) {
+        if (left >= right)
+            return;
+        int l = left;
+        int r = right;
+        int pivot = nums[l];    //pivot取出来,数组空一个位置，用来作为缓存，排序后再填充pivot
+        while (l < r) {
+            if (l < r && nums[r] > pivot) {
+                r--;
+            }
+            nums[l] = nums[r]; // 指针第一次所指位置在pivot，pivot已经取出来，可以覆盖      指针第2-n次所指位置是覆盖操作后的原元素
+            if (l < r && nums[l] <= pivot) {
+                l++;
+            }
+            nums[r] = nums[l];
+            //替换的两种情况
+            //1.r--,直到r = l, l指针所指位置为可覆盖位置
+            //2.l++,直到r = l, r指针所指位置为可覆盖位置
+            //又因为l = r;所以nums[l]=nums[r] =pivot
+        }
+        nums[r] = pivot;
+
+        quickSort(nums, left, l - 1);
+        quickSort(nums, l + 1, right);
+    }
+
 
     //    65 32 4
     void dps(int[] nums, int start, int end) {
@@ -62,7 +88,5 @@ public class 快速排序 {
 
         dps(nums, start, left - 1);
         dps(nums, left + 1, end);
-
-
     }
 }
